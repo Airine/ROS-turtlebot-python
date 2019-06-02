85b7eb9c4881f2f53257c65731382f28a1e82661
airine <airine0119@gmail.com>
2019-06-02 18:51:10 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+'''
+Copyright (c) 2019, Aaron Tian
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+import rospy
+from geometry_msgs.msg import Twist, String
+from math import radians
+
+mms = None
+
+moves = []
+times = []
+# 0: rotate, 1: go_circle, 2: cxk_left, 3: cxk_right, 3: tri_move, 4: tri_turn
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    if data.data == 'test':
+        for i in range(times(0)):
+            while rospy.is_shutdown():
+                mms.cmd_vel.publish(moves[0])
+                mms.r.sleep()
+
+class Movements():
+    
+    def __init__(self):
+        rospy.init_node('Movements', anonymous=False)
+        rospy.Subscriber('chatter', String, callback)
+        # rospy.loginfo("To stop TurtleBot CTRL + C")
+        rospy.on_shutdown(self.shutdown)
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        self.r = rospy.Rate(10) # 10Hz = 0.1s
+        rospy.spin()
+        
+
+    def init_cmds():
+        rotate_cmd = Twist()
+        rotate_cmd.linear.x = 0
+        rotate_cmd.angular.z = 0.5 # radians/s
+        moves.append(rotate_cmd)
+        times.append(20)
+
+        circle_cmd = Twist()
+        circle_cmd.linear.x = 0.2
+        circle_cmd.angular.z = 0.5
+        moves.append(circle_cmd)
+        times.append(30)
+
+        cxk_left = Twist()
+        cxk_left.linear.x = 0
+        cxk_left.angular.z = 1
+        moves.append(cxk_left)
+        times.append(2)
+
+        cxk_right = Twist()
+        cxk_right.linear.x = 0
+        cxk_right.angular.z = -1
+        moves.append(cxk_right)
+        times.append(2)
+        
+        tri_move = Twist()
+        tri_move.linear.x = 0.2
+        moves.append(tri_move)
+        times.append(10)
+
+        tri_turn = Twist()
+        tri_turn.linear.x = 0
+        tri_turn.angular.z = radians(60)
+        moves.append(tri_turn)
+        times.append(20)
+
+    def shutdown(self):
+        rospy.loginfo("Stop TurtleBot")
+        self.cmd_vel.publish(Twist())
+        rospy.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        mms = Movements()
+    except:
+        rospy.loginfo("Movements node terminated.")
---
+++ talker.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+import sys
+import select
+import termios
+import tty
+
+commands = {
+    'q': 'str',
+    'w': 'str',
+    't': 'test'
+}
+
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+if __name__ == '__main__':
+    settings = termios.tcgetattr(sys.stdin)
+    pub = rospy.Publisher('chatter', String, queue_size=1)
+    rospy.init_node('talker', anonymous=True)
+    try:
+        while not rospy.is_shutdown():
+            key = getKey()
+            if key in commands.keys():
+                cmd = commands[key]
+            else:
+                cmd = 'nothing'
+            print(cmd)
+            pub.publish(cmd)
+    except Exception as e:
+        print(e)
+    finally:
+        pub.publish('nothing')
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
